130143e818ab20bab12304a6cee5be03a3a206a1
N Karthika <nk00851725@techmahindra.com>
2026-09-01 20:15:12 +0530

    fix linting issue

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -1,15 +1,14 @@
-export default function decorate(block){
-    const rows = [...block.children];
+export default function decorate(block) {
+  const rows = [...block.children];
 
-    rows.forEach((row) => {
-        const cells = [...row.children];
-        if (cells.length < 2) return;
+  rows.forEach((row) => {
+    const cells = [...row.children];
+    if (cells.length < 2) return;
 
-        const image = cells[0];
-        const content = cells[1];
+    const image = cells[0];
+    const content = cells[1];
 
-        image.classList.add('banner-image');
-        content.classList.add('banner-content');
-
-    })
+    image.classList.add('banner-image');
+    content.classList.add('banner-content');
+  });
 }
