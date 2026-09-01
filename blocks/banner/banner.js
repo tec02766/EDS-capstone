5de0033e7cc152deeae32a723e03d00cbe4cc255
N Karthika <nk00851725@techmahindra.com>
2026-09-01 19:39:42 +0530

    custom block banner

---
+++ blocks/banner/banner.js
@@ -0,0 +1,15 @@
+export default function decorate(block){
+    const rows = [...block.children];
+
+    rows.forEach((row) => {
+        const cells = [...row.children];
+        if (cells.length < 2) return;
+
+        const image = cells[0];
+        const content = cells[1];
+        
+        image.classList.add('banner-image');
+        content.classList.add('banner-content');
+
+    })
+}
