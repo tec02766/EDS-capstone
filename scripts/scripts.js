11ad1d71dc8b92f87bf62b0f05999aaa12f72921
N Karthika <nk00851725@techmahindra.com>
2026-09-03 12:15:10 +0530

    add modal block

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -48,6 +48,17 @@ async function loadFonts() {
     // do nothing
   }
 }
+function autolinkModals(element) {
+  element.addEventListener('click', async (e) => {
+    const origin = e.target.closest('a');
+
+    if (origin && origin.href && origin.href.includes('/modals/')) {
+      e.preventDefault();
+      const { openModal } = await import(`${window.hlx.codeBasePath}/blocks/modal/modal.js`);
+      openModal(origin.href);
+    }
+  });
+}
 
 /**
  * Turns `/widgets/...` links into widget blocks.
@@ -200,7 +211,7 @@ async function loadEager(doc) {
  */
 async function loadLazy(doc) {
   loadHeader(doc.querySelector('body > header'));
-
+  autolinkModals(doc);
   const main = doc.querySelector('main');
   await loadSections(main);
 
